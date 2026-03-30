ffc8d7cfb8b0eb81bae7a9e710e1af82aa34413f
Nishtha-agarwal <93310872+Nishtha-agarwal@users.noreply.github.com>
2026-03-30 09:32:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def load_user(user_id):
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Welcome to Migri!"
 
 # Register
 @app.route("/api/register", methods=["POST"])
