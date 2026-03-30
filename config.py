3c6a1098f65b9e8a665d74c7fd47972bb650b85b
Nishtha-agarwal <93310872+Nishtha-agarwal@users.noreply.github.com>
2026-03-30 07:52:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey')
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL', f'sqlite:///{os.path.join(basedir, "app.db")}'
-    )
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "migri.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwtsecretkey')
